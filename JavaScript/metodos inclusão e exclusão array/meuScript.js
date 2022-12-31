760ec7dbd32f9d7f1ec5e0146329da79fb244488
Junior <adilson.juniorcomunicacao@gmail.com>
2022-12-31 15:49:27 -0300

    comite31_12

---
+++ JavaScript/metodos inclusão e exclusão array/meuScript.js
@@ -0,0 +1,29 @@
+ 
+
+ var lista_coisas = Array()
+
+ lista_coisas['frutas'] = Array()
+ lista_coisas['pessoas'] = Array()
+
+
+ lista_coisas['frutas'].push('Banana')
+ lista_coisas['frutas'].push('Uva')
+
+ console.log(lista_coisas)
+
+
+ //incluir um elemento no final do array
+
+ //ista_frutas.push('Uva')
+
+ //incluir um elemento no inicio de um array
+
+ //lista_frutas.unshift('Uva')
+
+ //excluir um elemento no final do array
+ //lista_frutas.pop()
+
+ //exluir um elemento no inicio do array 
+ //lista_frutas.shift()
+
+
