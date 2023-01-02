1b0c8f1ba6e3ab9fdf754dc30f8cf252e104e814
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-02 13:54:05 -0300

    comite02_01

--- JavaScript/Estrutura de repetiçao do_while/meuScript.js
+++ JavaScript/Estrutura de repetiçao do_while/meuScript.js
@@ -1,1 +1,9 @@
-alert("ola")
+var x = 11
+
+do{
+	document.write(x + '<br />')
+
+
+	//break 
+	//continue
+}while(x <= 10)
---
+++ JavaScript/Estutura de repetição_for/meuScript.js
@@ -0,0 +1,20 @@
+/*
+while(condicao){
+	
+}
+
+do{
+	
+
+
+}while(condica){
+	
+
+}
+*/
+
+for(var x = 10; x >= 0; x--){
+
+	document.write(x + '<br />'); 
+
+}
