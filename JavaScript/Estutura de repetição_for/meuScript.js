0585c91ddad6ed8df79e8b6a937aa244852605fd
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-08 21:01:19 -0300

    atualizacoes

--- JavaScript/Estutura de repetição_for/meuScript.js
+++ JavaScript/Estutura de repetição_for/meuScript.js
@@ -17,4 +17,6 @@ for(var x = 10; x >= 0; x--){
 
 	document.write(x + '<br />'); 
 
+	//breack;
+	//continue
 }
---
+++ JavaScript/Jogo do mosquisto - projeto/jogo.js
@@ -0,0 +1,16 @@
+
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanho(){
+
+	altura = window.innerHeight 
+	largura = window.innerWidth 
+
+	console.log(altura, largura);
+
+
+}
+
+ajustaTamanho()
