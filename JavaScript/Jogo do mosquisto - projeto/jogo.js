f94e2bb501963e4bbdf503c06780559c0640b437
Junior <adilson.juniorcomunicacao@gmail.com>
2023-01-08 21:31:45 -0300

    atualizacoes

--- JavaScript/Jogo do mosquisto - projeto/jogo.js
+++ JavaScript/Jogo do mosquisto - projeto/jogo.js
@@ -8,9 +8,26 @@ function ajustaTamanho(){
 	altura = window.innerHeight 
 	largura = window.innerWidth 
 
-	console.log(altura, largura);
-
-
+	console.log(largura, altura);
 }
 
 ajustaTamanho()
+
+function posicaoRandomica(){
+
+	var posicaoX = Math.floor(Math.random() * largura)
+	var posicaoY = Math.floor(Math.random() * altura)
+
+	console.log(posicaoX, posicaoY) 
+
+
+	//criar o elementos html a imagem
+	var mosquito = document.createElement('img')
+
+	mosquito.scr = 'imagens/coracao_cheio.png'	
+
+	mosquito.className = 'mosquito1'
+
+	document.body.appendChild(mosquito)
+
+}
