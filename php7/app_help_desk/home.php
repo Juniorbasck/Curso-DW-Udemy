d6f74bafc19568292554479133d44bf6fb91ca04
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-04-13 18:03:12 +0100

    atualizacoes

--- php7/app_help_desk/home.php
+++ php7/app_help_desk/home.php
@@ -1,5 +1,7 @@
 <?php 
-    require_once "validador_acesso.php"
+    require_once "validador_acesso.php";
+
+    
 ?>
 <html>
   <head>
--- php7/app_help_desk/valida_login.php
+++ php7/app_help_desk/valida_login.php
@@ -4,12 +4,17 @@
 
     //verifica se autenticação foi realizada
     $usuarios_autenticado = false; 
+    $usuarios_id = null;
+    $usuarios_perfil_id = null;
 
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuário');
 
     //usuarios do sistemas
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234','perfil_id' => 1),
+        array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+        array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
     );  
 
     
@@ -25,14 +30,16 @@
         
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
             $usuarios_autenticado = true;
+            $usuarios_id = $user['id'];
+            $usuarios_perfil_id = $user['perfil_id'];
         }
     };
 
     if($usuarios_autenticado){
         echo 'Usuário autenticado';
         $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['x'] = 'um valor';
-        $_SESSION['y'] = 'outro valor';   
+        $_SESSION['id'] = $usuarios_id;
+        $_SESSION['perfil_id'] = $usuarios_perfil_id;
         header('Location: home.php');
     }else{
         $_SESSION['autenticado'] = 'NÃO';
