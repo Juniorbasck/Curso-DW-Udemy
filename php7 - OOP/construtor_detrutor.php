5738e6ea671654857e374f1a56ba3e20753eff82
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-05-05 16:15:14 +0100

    atualizacoes

---
+++ php7 - OOP/construtor_detrutor.php
@@ -0,0 +1,32 @@
+<?php
+    
+    class Pessoa{
+        
+        public $nome = null;
+
+        function __construct($nome){
+            echo 'Objeto iniciado';
+            $this->nome = $nome;
+            
+        }
+
+        function __destruct(){
+            echo 'Objeto removido';
+        }
+
+        function  __get($atributo){
+            return $this->$atributo;
+        }
+
+        function Correr(){
+            return $this->__get('nome') . ' está correndo';
+        }
+    }   
+
+    $pessoa = new Pessoa('Junior');
+    echo '<br />Nome: ' . $pessoa->__get('nome');
+    echo '<br />' . $pessoa->Correr();
+
+    echo '<br />';
+    // unset($pessoa);
+?>
