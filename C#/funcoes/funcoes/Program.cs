e602cb1d77592e81f94066b594905f33d708d57c
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-08-24 21:08:11 -0300

    funcoes

--- C#/funcoes/funcoes/Program.cs
+++ C#/funcoes/funcoes/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Course
+{
+    class program
+    {   
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite três numeros:");
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double reusltado = Maior(n1,n2, n3);
+
+            Console.WriteLine("Maior = " + reusltado);
+        }
+
+        static int Maior(int a, int b, int c)  {
+            int m;
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+       
+        }
+    }
+}
