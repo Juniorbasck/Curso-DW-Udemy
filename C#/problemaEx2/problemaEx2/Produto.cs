b2b00ea6037fd23e8e262d34b3369e13444393c9
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-08-30 00:33:33 -0300

    fazer exercicio

---
+++ C#/problemaEx2/problemaEx2/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemaEx2
+{
+    internal class Produto
+    {
+
+        public string nome;
+
+        public double Preco;
+
+        public int Quantidade; 
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade; 
+        }
+    }
+}
---
+++ C#/problemaEx2/problemaEx2/Program.cs
@@ -0,0 +1,30 @@
+﻿using problemaEx2;
+using System;
+using System.Threading.Channels;
+using System.Globalization;
+
+namespace Course
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+
+            Console.WriteLine("nome: ");
+
+            p.nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
