ed5d570c7f29966b88b8bcd7a03b207dc1e6a9a4
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-08-31 01:10:30 -0300

    membrosStaticos2

--- C#/membrosEstaticos/membrosEstaticos/Program.cs
+++ C#/membrosEstaticos/membrosEstaticos/Program.cs
@@ -1,33 +1,26 @@
-﻿using System;
+﻿using membrosEstaticos;
+using System;
 using System.Globalization;
 
 namespace Course
 {
     class Program
     {
-        static double Pi = 3.14;
+       
         static void Main(string[] args)
         {
-
+       
             Console.WriteLine("Entre com o valor: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double Circ = Circuferencia(raio);
-            double volume = Volume(raio);
+            double Circ = Calculadora.Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circuferencia: " + Circ);
             Console.WriteLine("Circuferencia: " + Circ);
-            Console.WriteLine("Valor de PI: " + Pi);
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi);
         }
 
-        static double Circuferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume( double raio)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3);
-        }
+        
     }
 }
