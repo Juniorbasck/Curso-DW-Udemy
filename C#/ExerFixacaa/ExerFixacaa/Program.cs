4a68f8005d408f9dd2474cbe6bc422447b0f9852
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-09-06 01:23:10 -0300

    atualizcoes

---
+++ C#/ExerFixacaa/ExerFixacaa/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace ExerFixacaa
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+             
+            ContaBancaria conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string tiular = Console.ReadLine();
+
+            Console.Write("Haverá depósito incial (S/N)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Entre o valor de depósito inicial: ");
+                Double depositoInical = double.Parse(Console.ReadLine());
+
+                conta = new ContaBancaria(numero, tiular, depositoInical);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, tiular);
+            }
+
+        }
+    }
+}
