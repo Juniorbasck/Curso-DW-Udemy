bc046ea372c95af6c44b4f4ddc0ed4e19205c3e5
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-11-25 22:54:43 -0300

    finazalizando modulo

---
+++ C#/2/ExDemoBuilder/ExDemoBuilder/Entities/Post.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExDemoBuilder.Entities
+{
+    internal class Post
+    {
+
+        public DateTime Moment { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int Likes { get; set; }
+
+        public List<Commennt> Commennts{ get; set;} = new List<Commennt>();
+
+        public Post() 
+        {
+        
+        }
+
+        public Post(DateTime moment, string title, string content, int likes, List<Commennt> commennts)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Commennt comment)
+        {
+            Commennts.Add(comment);
+        }
+
+        public void RemoveComment(Commennt comment)
+        {
+            Commennts.Remove(comment);
+        }
+    }
+}
