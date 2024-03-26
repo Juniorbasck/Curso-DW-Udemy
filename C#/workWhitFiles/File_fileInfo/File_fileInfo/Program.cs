ac9b98113db2de47eba37e9f5be41708688f1cf0
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-03-25 23:30:31 -0300

    working whit files

---
+++ C#/workWhitFiles/File_fileInfo/File_fileInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+class  program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = "/home/Adilson/Pessoal/Estudos/Curso-DW-Udemy/C#/workWhitFiles/Usesfiles/teste.txt";
+        
+        string targetpath = "~/Adilson/Pessoal/Estudos/Curso-DW-Udemy/C#/workWhitFiles/Usesfiles/teste2.txt";
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            fileInfo.CopyTo(targetpath);
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine("an error occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
