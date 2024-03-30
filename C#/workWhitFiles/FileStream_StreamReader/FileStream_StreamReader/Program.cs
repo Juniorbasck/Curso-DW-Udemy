6159f3d55319bb2f4d8cc043304211f005d4eea9
junior <adilson.juniorocomunicacao@gmail.com>
2024-03-29 23:39:58 -0300

    atualizacoes

---
+++ C#/workWhitFiles/FileStream_StreamReader/FileStream_StreamReader/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+
+class Program
+{
+    static void main(string[] args)
+    {
+        string path = "/home/junior/Documentos/file1";
+
+        FileStream fs = null;
+        StreamReader sr = null;
+
+        try
+        {
+            fs = new FileStream(path, FileMode.Open);
+            sr = new StreamReader(fs);
+
+            string line = sr.ReadLine();
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error ocurred");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if ( sr !=  null) sr.Close();
+            if ( fs != null) fs.Close();
+        }
+    }
+}
